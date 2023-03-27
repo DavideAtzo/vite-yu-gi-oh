f9a338ef077c4ed08e76245c7ddb9c3911657c1e
DavideAtzo <atzo9477@gmail.com>
2023-03-27 15:51:09 +0200

    logo + fine header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,9 +7,24 @@ export default {
 <template>
     <header>
         <div class="container-fluid">
-            <div><h1>Yu-GiOh Api</h1></div>
+            <div id="section-title" class="row align-items-center">
+                <div class="col-auto"><img src="/img/logo-Yu-Gi-Oh.png" alt=""></div>
+                <div class="col-auto">
+                    <h1>Yu-GiOh Api</h1>
+                </div>
+            </div>
         </div>
     </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+    #section-title {
+        height: 100px;
+
+        img {
+            width: 75px;
+        }
+    }
+
+}</style>
