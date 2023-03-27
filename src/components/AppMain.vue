756cde514cc75a8a8426a9dc6e2588c8ef05d4ae
DavideAtzo <atzo9477@gmail.com>
2023-03-27 16:00:28 +0200

    background color main

---
+++ src/components/AppMain.vue
@@ -0,0 +1,23 @@
+<script>
+    export default {
+        
+    }
+</script>
+
+
+<template>
+   <main>
+    
+   </main>
+</template>
+
+
+
+<style lang="scss" scoped>
+@use '../assets/scss/variables/variables.scss'  as *;
+
+main{
+    background-color: $primary;
+    height: calc(100vh - 100px);
+}
+</style>
